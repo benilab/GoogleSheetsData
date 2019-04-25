bf3545c225c36af05198376569b49050a52b3019
afrobayofranco <morewebdesignrocks@gmail.com>
2019-04-24 21:39:39 -0400

    Separate results management from authentication
    Functions related to the insertion of results into the document are
    separated on a new script (gconnection.js)

--- gconnection.js
+++ gconnection.js
@@ -1,82 +1,30 @@
-// Client ID and API key from the Developer Console
-var CLIENT_ID =
-  '933014663888-9hf81boiqu4cmm0v36btohfr7rlvn9kc.apps.googleusercontent.com'
-var API_KEY = 'AIzaSyDDn9V69B0F39JfagK5xGO11FFUbSjIFUw'
-
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = [
-  'https://sheets.googleapis.com/$discovery/rest?version=v4',
-]
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-
-var authorizeButton = document.getElementById('authorize_button')
-var signoutButton = document.getElementById('signout_button')
-
 /**
- *  On load, called to load the auth2 library and API client library.
+ * Console log content from googlesheet
  */
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient)
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-  gapi.client
-    .init({
-      apiKey: API_KEY,
-      clientId: CLIENT_ID,
-      discoveryDocs: DISCOVERY_DOCS,
-      scope: SCOPES,
+function consoleloglistMajors() {
+  gapi.client.sheets.spreadsheets.values
+    .get({
+      spreadsheetId: '1hBfH9vTXqV8CobzBlSHaKBec39t0gB5cyRMPhXUU0YY',
+      range: 'Sheet1!A1:A5',
     })
     .then(
-      function() {
-        // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus)
-
-        // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get())
-        authorizeButton.onclick = handleAuthClick
-        signoutButton.onclick = handleSignoutClick
+      function(response) {
+        var range = response.result
+        if (range.values.length > 0) {
+          /*appendPre('Name, Major:')*/
+          for (i = 0; i < range.values.length; i++) {
+            var row = range.values[i]
+            // Print columns A and E, which correspond to indices 0 and 4.
+            console.log('This is the result' + row[0] + ', ' + row[4])
+          }
+        } else {
+          console.log('No data found.')
+        }
       },
-      function(error) {
-        appendPre(JSON.stringify(error, null, 2))
+      function(response) {
+        console.log('Error: ' + response.result.error.message)
       }
     )
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    authorizeButton.style.display = 'none'
-    signoutButton.style.display = 'block'
-    listMajors()
-  } else {
-    authorizeButton.style.display = 'block'
-    signoutButton.style.display = 'none'
-  }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-  gapi.auth2.getAuthInstance().signIn()
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-  gapi.auth2.getAuthInstance().signOut()
 }
 
 /**
@@ -120,9 +68,3 @@ function listMajors() {
       }
     )
 }
-
-async
-defer
-src = 'https://apis.google.com/js/api.js'
-onload = 'this.onload=function(){};handleClientLoad()'
-onreadystatechange = "if (this.readyState === 'complete') this.onload()"
